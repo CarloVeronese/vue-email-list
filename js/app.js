9038201776adb27eb74a496e7d5955a6b96ecaec
CarloVeronese <carlo.veronese97@gmail.com>
2023-11-02 15:07:03 +0100

    Add - generate and print 10 emails with API

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+const emailDOMElement = document.querySelector('.email-list');
+
 for(let i = 0; i < 10; i++){
     axios
     .get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then((res) => {
-        console.log(res.data.response);
+        newMail = res.data.response;
+        emailDOMElement.innerHTML += `<li>${newMail}</li>`
     })
 }
